fcddc494acdbc5168f4ed775439e5aa5b7eafb61
ASUS <5646krupaparmar@gmail.com>
2024-06-08 05:47:45 +0530

    3/6/2024 krupa 3-6-2024 5/6/2024 6/6/2024 7/6/24 8/6/24

--- lib/screens/Dialog Box/dialog_box.dart
+++ lib/screens/Dialog Box/dialog_box.dart
@@ -13,37 +13,47 @@ class _dialogPageState extends State<dialogPage> {
   Widget build(BuildContext context) {
     return SafeArea(child:
     Scaffold(
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
         centerTitle: true,
         title: Text('Dialog Box',style: TextStyle(
-          fontWeight: FontWeight.w800
+          fontWeight: FontWeight.w800,
+          color: Colors.white
         ),),
       ),
       body: Column(
+
         children: [
-          ElevatedButton(onPressed: () {
-            showDialog(context: context, builder: (context) {
-              return Dialog(
-                child: Container(
-                  height: 300,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      FlutterLogo(size: 150,),
-                      Text("This is a Dialog Box",style:TextStyle(fontSize: 20),),
-                      ElevatedButton(
 
-                          onPressed: (){
-                            Navigator.of(context).pop();
-                          }, child: Text("Back"))
-                    ],
+          SizedBox(
+            height: 50,
+          ),
+          Center(
+            child: ElevatedButton(onPressed: () {
+              showDialog(context: context, builder: (context) {
+                return Dialog(
+                  child: Container(
+                    height: 300,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        FlutterLogo(size: 150,),
+                        Text("This is a Dialog Box",style:TextStyle(fontSize: 20),),
+                        ElevatedButton(
+
+                            onPressed: (){
+                              Navigator.of(context).pop();
+                            }, child: Text("Back"))
+                      ],
+                    ),
                   ),
-                ),
+                );
+              },
               );
-            },
-            );
 
-          }, child: Text('Dialog Box')),
+            }, child: Text('Dialog Box')),
+          ),
           SizedBox(
             height: 50,
           ),
@@ -66,13 +76,16 @@ class _dialogPageState extends State<dialogPage> {
                              Navigator.of(context).pop();
                            }, child: Text("Back")),
                        SizedBox(
-                         width: 50,
+                         width: 40,
                        ),
-                       ElevatedButton(
+                       Padding(
+                         padding: const EdgeInsets.all(8.0),
+                         child: ElevatedButton(
 
-                           onPressed: (){
-                             Navigator.of(context).pop();
-                           }, child: Text("Save"))
+                             onPressed: (){
+                               Navigator.of(context).pop();
+                             }, child: Text("Save")),
+                       )
                      ],
                    )
                   ],
@@ -83,8 +96,12 @@ class _dialogPageState extends State<dialogPage> {
             );
 
           }, child: Text('AlertDialog Box')),
+          SizedBox(
+            width: 50,
+            height: 50,
+          ),
           ElevatedButton(
-              child: Text("CupertinoAlertDialog-1"),
+              child: Text("CupertinoAlertDialog-part 1"),
               onPressed:(){
                 showDialog(context: context, builder: (BuildContext context){
                   return CupertinoAlertDialog(
@@ -104,19 +121,21 @@ class _dialogPageState extends State<dialogPage> {
               }
           ),
           SizedBox(
-            height: 20,
+            height: 50,
           ),
           ElevatedButton(
-              child: Text("CupertinoAlertDialog-2"),
+              child: Text("CupertinoAlertDialog-part 2"),
               onPressed:(){
                 showDialog(context: context, builder: (BuildContext context){
                   return CupertinoAlertDialog(
                     title: Text("Success"),
                     actions: [
                       CupertinoDialogAction(onPressed: (){
+
                         Navigator.of(context).pop();
                       }, child: Text("Back")),
                       CupertinoDialogAction(onPressed: (){
+
                         Navigator.of(context).pop();
                       }, child: Text("Next")),
                       CupertinoDialogAction(onPressed: (){
@@ -131,7 +150,7 @@ class _dialogPageState extends State<dialogPage> {
               }
           ),
           SizedBox(
-            height: 20,
+            height: 50,
           ),
 ElevatedButton(onPressed: () {
   showDialog(context: context, builder:(BuildContext context){
@@ -142,6 +161,13 @@ ElevatedButton(onPressed: () {
           child: Column(
             children: [
               FlutterLogo(size: 100,),
+    SizedBox(height: 30,),
+    ElevatedButton(
+
+    onPressed: (){
+    Navigator.of(context).pop();
+    }, child: Text("Back")),
+
 
             ],
           ),
@@ -152,42 +178,7 @@ ElevatedButton(onPressed: () {
   );
 
 }, child: Text('Full Page'))
-          // ElevatedButton(onPressed: () {
-          //   showDialog(context: context, builder: (context) {
-          //     return Custom(
-          //       title: Container(
-          //         height: 200,
-          //         child: Column(
-          //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //           children: [
-          //
-          //             Text("This is a AlertDialog Box",style:TextStyle(fontSize: 20),),
-          //             Text("Success",style:TextStyle(fontSize: 20),),
-          //             Row(
-          //               children: [
-          //                 ElevatedButton(
-          //
-          //                     onPressed: (){
-          //                       Navigator.of(context).pop();
-          //                     }, child: Text("Back")),
-          //                 SizedBox(
-          //                   width: 50,
-          //                 ),
-          //                 ElevatedButton(
-          //
-          //                     onPressed: (){
-          //                       Navigator.of(context).pop();
-          //                     }, child: Text("Save"))
-          //               ],
-          //             )
-          //           ],
-          //         ),
-          //       ),
-          //     );
-          //   },
-          //   );
-          //
-          // }, child: Text(' Custom dialog Box'))
+
         ],
       ),
     ));
